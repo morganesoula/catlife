a961b435573d6dabe916ab86c71904b6b7447a40
Morgane Soula <morganes56@gmail.com>
2022-01-20 19:13:54 +0100

    Finished splashscreen

---
+++ app/src/main/java/com/ms/catlife/splashscreen/Splashscreen.kt
@@ -0,0 +1,36 @@
+package com.ms.catlife.splashscreen
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.AttributeSet
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ms.catlife.MainActivity
+import com.ms.catlife.R
+
+class Splashscreen : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }, 2500)
+    }
+}
